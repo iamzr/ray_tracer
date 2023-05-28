528a8084af959c51f2a34e6301a5559a3064d0ac
Zia Rehman <zia.rehman@scanmarqed.com>
2023-05-28 15:34:19 +0100

    Added Image struct

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,34 @@
+struct Image {
+    width: i32,
+    height: i32,
+}
+
 fn main() {
+
     // Image
 
-    const IMAGE_WIDTH: i32 = 256;
-    const IMAGE_HEIGHT: i32 = 256;
+    const IMAGE: Image = Image {
+        width: 256,
+        height: 256
+    };
     
     // Render
 
     println!("P3
     {} {}
-    255", IMAGE_WIDTH, IMAGE_HEIGHT);
+    255", IMAGE.width, IMAGE.height);
 
-    for j in (0..IMAGE_HEIGHT).rev() {
-        for i in 0..IMAGE_WIDTH {
-            let r= i as f64 / (IMAGE_WIDTH- 1) as f64;
-            let g= j as f64 / (IMAGE_HEIGHT- 1) as f64;
+    for j in (0..IMAGE.height).rev() {
+        for i in 0..IMAGE.width{
+            let r= i as f32 / (IMAGE.width- 1) as f32;
+            let g= j as f32 / (IMAGE.height- 1) as f32;
             let b= 0.25;
 
             // println!("{} {} {}", r, g, b);
 
-            let ir = (255.999 as f64 * r) as i64;
-            let ig = (255.999 as f64 * g) as i64 ;
-            let ib = (255.999 as f64 * b) as i64 ;
+            let ir = (255.999 as f32 * r) as i32;
+            let ig = (255.999 as f32 * g) as i32 ;
+            let ib = (255.999 as f32 * b) as i32 ;
 
             println!("{} {} {}", ir, ig, ib);
         }
